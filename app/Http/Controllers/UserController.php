ca0ce0d9ec4305d9f1b46edb8a00e8f5c2c95ae0
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-03-13 21:37:20 +0700

    praktikum 1 jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,9 +14,12 @@ class UserController extends Controller
     {
         //tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelangan Pratama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'manager 3',
+            'password' => Hash::make(12345)
         ];
-        UserModel::where('username', 'customer-1')->update($data); //tambahkan data ke tabel m_user
+        UserModel::create($data); //tambahkan data ke tabel m_user
 
         //coba akses UserModel
         $user =UserModel::all();//ambil semua data dari tabel user
