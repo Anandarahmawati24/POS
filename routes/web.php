4aee8d1fa6edd31b26de74d3ac1311bc4d616c0e
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-03-15 11:03:49 +0700

    2.6 create

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ use App\Http\Controllers\SalesController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
 
 //route halaman home
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -42,3 +42,5 @@ Route::get('/sales', [SalesController::class, 'index']);
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah'])->name('user_tambah');
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
