c656fe1aeb97a855f07b64f891fd9b907c6c9364
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-03-15 11:20:25 +0700

    2.6 ubah

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,4 +29,9 @@ class UserController extends Controller
         ]);
         return redirect('/user');
     }
+
+    public function ubah($id){
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
 }
