84642da76902288a68561638f8206b5631857f6f
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-03-21 19:45:54 +0700

    ajax done

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,8 @@ Route::group(['prefix' => 'user'], function () {
   Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
   Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // menampilkan halaman form edit user
   Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // menyimpan perubahan data user
+  Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);// tampilan form delete user ajax
+  Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);// hapus data user ajax
   Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
 });
 
