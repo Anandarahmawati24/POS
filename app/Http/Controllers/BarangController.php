62be0f180e9e42639c10019af5ed4c83b8d99852
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-05-12 10:57:33 +0700

    export pdf menu barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -7,6 +7,7 @@ use App\Models\KategoriModel;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
@@ -408,4 +409,20 @@ public function export_excel(){
     $writer->save('php://output');
     exit;
 } // end function export_excel
+
+public function export_pdf(){
+    $barang = BarangModel::select('kategori_id','barang_kode','barang_nama','harga_beli','harga_jual')
+    ->orderBy('kategori_id')
+    ->orderBy('barang_kode')
+    ->with('kategori')
+    ->get();
+
+    //use Barryvdh\DomPDF\Facade\Pdf;
+    $pdf = Pdf::loadView('barang.export_pdf', ['barang' => $barang]);
+    $pdf->setPaper('a4','potrait'); //set ukuran kertas dan orientasi
+    $pdf->setOption("isRemoteEnabled", true); //set true jika ada gambar dari url
+    $pdf->render();
+
+    return $pdf->stream('Data Barang '.date('Y-m-d H:i:s').'.pdf');
+}
 }
