8b044c40b8eae61657fef5b2a4b00960398b69e9
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-05-31 21:08:10 +0700

    jobsheet 10 API praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,40 +6,30 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'username',
+        'nama',
+        'password',
+        'level_id'
+    ];
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }  
+
+    public function getJWTCustomClaims()
+    {
+        
+        return[];
+    }
    protected $table = 'm_user'; // mendefinisikan nama tabel yang digunakan oleh model ini
    protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
 
-   protected $fillable = ['level_id','username','password','nama','foto'];
-
-   protected $hidden = ['password']; //jangan ditampilkan saat select
-   protected $casts = ['password' => 'hashed']; //casting password agar otomatis dihash
-
-   public function level(): BelongsTo{
-    return $this->belongsTo(LevelModel:: class, 'level_id', 'level_id');
-   }
-   /**
-    * mendapatkan nama role
-    */
-    public function getRoleName(): string {
-        return $this->level->level_nama;
-    }
-
-    /**
-    * cek apakah user memiliki role tertentu
-    */
-    public function hasRole($role): bool {
-        return $this->level->level_kode == $role;
-    }
-
-    /**
-    * mendapatkan kode role
-    */
-    public function getRole() {
-        return $this->level->level_kode;
-}
 }
