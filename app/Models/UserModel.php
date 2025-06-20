9d83e91f630df0cc6ab356d09709bd8c13ec7a9e
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-06-20 20:03:06 +0700

    Jobshet 11 Restfull API 2, Alhamdulillah PWL Finished hehe

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class authenticatable
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
@@ -16,7 +17,8 @@ class UserModel extends Authenticatable implements JWTSubject
         'username',
         'nama',
         'password',
-        'level_id'
+        'level_id',
+        'image'
     ];
 
     public function getJWTIdentifier()
@@ -31,5 +33,20 @@ class UserModel extends Authenticatable implements JWTSubject
     }
    protected $table = 'm_user'; // mendefinisikan nama tabel yang digunakan oleh model ini
    protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
+
+    public function level()
+    {
+        return $this->belongsTo(
+            LevelModel::class,
+            'level_id',
+            'level_id'
+        );
+    }
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
+    }
 
 }
